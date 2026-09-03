5080aa70142a2619539807155b981cd2e9897a34
philiphabib <philip.habib1@yahoo.com>
2026-09-03 23:33:35 +0300

    working package

---
+++ charts/open5gs-upf/resources/k8s-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo "Executing k8s customized entrypoint.sh"
+
+{{- range .Values.config.subnetList }}
+{{- if .createDev }}
+echo "Creating net device {{ .dev }}"
+if grep "{{ .dev }}" /proc/net/dev > /dev/null; then
+    echo "Warning: Net device {{ .dev }} already exists! may you need to set createDev: false";
+    exit 1
+fi
+
+ip tuntap add name {{ .dev }} mode tun
+ip link set {{ .dev }} up
+ip link set dev {{ .dev }} txqueuelen 4096
+echo "Setting IP {{ .gateway }} to device {{ .dev }}"
+ip addr add {{ .gateway }}/{{ .mask }} dev {{ .dev }};
+sysctl -w net.ipv4.ip_forward=1;
+{{- if .enableNAT }}
+echo "Enable NAT for {{ .subnet }} and device {{ .dev }}"
+iptables -t nat -A POSTROUTING -s {{ .subnet }} ! -o {{ .dev }} -j MASQUERADE;
+{{- end }}
+{{- end }}
+{{- end }}
+
+$@
